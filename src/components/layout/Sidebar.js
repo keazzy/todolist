4eff09187f92cb3530642aee9b218f0a850b99ec
Balogun Abdulazeez Adeleke <52777282+keazzy@users.noreply.github.com>
2020-03-27 19:13:48 +0100

    Updated Sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {FaChevronDown, FaInbox, FaRegCalenderAlt, FaCalenderReg, FaRegCalendar, FaRegCalendarAlt} from 'react-icons/fa';
+import {
+    FaChevronDown, 
+    FaInbox, 
+    FaRegCalenderAlt, 
+    FaCalenderReg, 
+    FaRegCalendar, 
+    FaRegCalendarAlt
+} from 'react-icons/fa';
 
 export const Sidebar = () => 
 <div className="sidebar" data-testid="sidebar">
     <ul className="sidebar__generic">
-        <li><span><FaInbox /></span><span>Inbox</span></li>
-        <li><span><FaRegCalendar/></span><span>Today</span></li>
+        <li>
+            <span><FaInbox /></span>
+            <span>Inbox</span>
+        </li>
+        <li>
+            <span><FaRegCalendar/></span>
+            <span>Today</span>
+        </li>
         <li><span><FaRegCalendarAlt/></span><span>Next 7 days</span></li>
     </ul>
+
+    <div className="sidebar__middle">
+        <span>
+            <FaChevronDown/>
+        </span>
+        <h2>Projects</h2>
+    </div>
+
+    <ul className="sidebar__projects"></ul>
+    Add Projects Components Here
 </div>
