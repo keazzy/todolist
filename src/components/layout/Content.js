290c46bbcbfbc4dab61828a3c9d7379132d24f12
Balogun Abdulazeez Adeleke <52777282+keazzy@users.noreply.github.com>
2020-03-27 15:59:06 +0100

    My first react app (todolist)

---
+++ src/components/layout/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Header} from './Header';
+import {Sidebar} from './Sidebar';
+
+export const Content = () => (
+    <section>
+        <Sidebar />
+    </section>
+);
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Sidebar = () => <p>i am a sidebar</p>
