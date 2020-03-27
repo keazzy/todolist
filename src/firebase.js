e50d58da7b57e3a539a65f731e9803593ce7b084
Balogun Abdulazeez Adeleke <52777282+keazzy@users.noreply.github.com>
2020-03-27 20:09:20 +0100

    Added firebase.js, included new dependecies for firebase, node-sass

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = firebse.intializeApp({
+    apikey = '',
+    authDomain = '',
+    databaseURL = '',
+    projectId = '',
+    storageBucket = '',
+    messageSenderId = '',
+    apiId = '',
+});
+
+export { firebaseConfig as firebase };
