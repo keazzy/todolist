9e7a9865a12451c02d44a61a741e143f71d0ed9c
Balogun Abdulazeez Adeleke <52777282+keazzy@users.noreply.github.com>
2020-03-27 16:21:14 +0100

    Upadate Sidebar.js

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import {FaChevronDown, FaInbox, FaRegCalenderAlt, FaCalenderReg, FaRegCalendar, FaRegCalendarAlt} from 'react-icons/fa';
 
-export const Sidebar = () => <p>i am a sidebar</p>
+export const Sidebar = () => 
+<div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+        <li><span><FaInbox /></span><span>Inbox</span></li>
+        <li><span><FaRegCalendar/></span><span>Today</span></li>
+        <li><span><FaRegCalendarAlt/></span><span>Next 7 days</span></li>
+    </ul>
+</div>
