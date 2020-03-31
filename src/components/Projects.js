e8cc74c30826e39ca6d516400cb6cb3fccf0810c
Balogun Abdulazeez Adeleke <52777282+keazzy@users.noreply.github.com>
2020-04-01 00:45:49 +0100

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,5 +3,34 @@ import React, {useState} from 'react';
 import {useSelectedProjectValue, useProjectsValue} from '../context';
 
 export const Projects = () => {
-    const []
+    const [active , setActive] = useState(activeValue);
+    const { setSelectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectsValue();
+
+    return (
+        projects &&
+        projects.map(project => (
+            <li 
+                key={project.projectId}
+                data-doc-id={projects.docId}
+                data-testId= "project-action"
+                role="button"
+                className={
+                    active === project.projectId
+                    ? 'active sidebar__project'
+                    : 'sidebar__project'
+                }
+                
+                onKeyDown   ={() => {
+                    setActive(Project.projectId);
+                    setSelectedProject(project.projectId);
+                }}
+
+                onClick={() => {
+                    setActive(Project.projectId);
+                    setSelectedProject(project.projectId);
+                }}
+            >I am a project</li>
+        ))
+    )
 }
